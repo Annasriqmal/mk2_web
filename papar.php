057eeb7cc8edfc97bb7ad95cb754e1c3e2c72b88
amirrasyid773 <143983278+amirrasyid773@users.noreply.github.com>
2023-09-05 17:02:30 +0800

    papar.php

--- papar.php
+++ papar.php
@@ -1,29 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Paparan</title>
+    <title>Cerita Saya</title>
 </head>
 <body>
     <?php
-        if (isset($_POST['message'])) 
-        {
-            $input_string = $_POST['message'];
 
-            $sentences = preg_split('/[.!?]|( )+/', $input_string, -1, PREG_SPLIT_NO_EMPTY);
-            $sentence_count = count($sentences);
-            echo "<p>Ayat ini mengandungi $sentence_count perkataan</p>";
+    $text=$_POST['message'];
 
-            $letter_count = strlen(preg_replace('/[^a-zA-Z]/', '', $input_string));
-            echo "<p>Ayat ini mengandungi $letter_count huruf</p>";
-
-            $reversed_string = strrev($input_string);
-            echo "<p>Ayat terbalik: $reversed_string</p>";
-        } 
-        else 
-        {
-            echo "<p>Tiada input</p>";
-        }
+    $wordcount=str_word_count($text);
+    $alphabetcount=strlen($text);
+    $reversed=strrev($text);
     ?>
+
+    <p>Ayat ini mengandungi <?php echo $wordcount; ?> patah perkataan</p>
+    <p>Ayat ini mengandungi <?php echo $alphabetcount; ?> huruf</p>
+    <p>Ayat terbalik: <?php echo $reversed; ?></p>
 
 </body>
 </html>
