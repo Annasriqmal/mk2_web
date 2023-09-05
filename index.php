dab9f3fda894e6e792bf6db0537422994b5e7f5c
Annasriqmal <143573860+Aininsofi@users.noreply.github.com>
2023-09-05 16:14:09 +0800

    siap
    siap

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Textarea Form</title>
+</head>
+<body>
+    <form method="post" action="papar.php">
+        <label for="message">Ceritakan tentang diri anda:</label><br>
+        <textarea id="message" name="message" rows="5" cols="50"></textarea><br><br>
+        <input type="submit" value="Simpan">
+    </form>
+</body>
+</html>
