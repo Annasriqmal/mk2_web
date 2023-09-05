586ccd24b3726fd9d9337fbf286fa9dde45ee753
amirrasyid773 <143983278+amirrasyid773@users.noreply.github.com>
2023-09-05 17:03:44 +0800

    papar.php

--- papar.php
+++ papar.php
@@ -3,19 +3,17 @@
 <head>
     <title>Cerita Saya</title>
 </head>
-<body>
-    <?php
-
-    $text=$_POST['message'];
-
-    $wordcount=str_word_count($text);
-    $alphabetcount=strlen($text);
-    $reversed=strrev($text);
-    ?>
-
-    <p>Ayat ini mengandungi <?php echo $wordcount; ?> patah perkataan</p>
-    <p>Ayat ini mengandungi <?php echo $alphabetcount; ?> huruf</p>
-    <p>Ayat terbalik: <?php echo $reversed; ?></p>
-
-</body>
+    <body>
+        <?php
+        $text=$_POST['message'];
+    
+        $wordcount=str_word_count($text);
+        $alphabetcount=strlen($text);
+        $reversed=strrev($text);
+        ?>
+    
+        <p>Ayat ini mengandungi <?php echo $wordcount; ?> patah perkataan</p>
+        <p>Ayat ini mengandungi <?php echo $alphabetcount; ?> huruf</p>
+        <p>Ayat terbalik: <?php echo $reversed; ?></p>
+    </body>
 </html>
